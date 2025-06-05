87fb7a3c55cf045ec1907f37b62c282abb978613
chip <163887314+nhatanhaxtanh@users.noreply.github.com>
2025-06-05 23:23:34 +0700

    WIP: commit tạm để checkout branch khác

--- frontend/src/pages/about/Team.tsx
+++
@@ -1,7 +0,0 @@
-export default function Team() {
-  return (
-    <div className="">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae inventore earum minus dolorum amet repudiandae, quos at ab quod commodi.
-    </div>
-  )
-}
--- frontend/src/pages/pricing/Services.tsx
+++
@@ -1,7 +0,0 @@
-export default function Services() {
-  return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quod, optio ipsa nesciunt veritatis ab dolorem voluptatum provident doloribus aliquam?
-    </div>
-  )
-}
