930a813ecb2ff0dc1eb7b7495080a6964f32a0f4
Truong Le Huy Hoang <hoanghuypeter1182004@gmail.com>
2025-06-08 21:15:17 +0700

    fix: check phoneRegex and passwordRegex first

--- frontend/src/lib/validations/auth.ts
+++ frontend/src/lib/validations/auth.ts
@@ -7,15 +7,15 @@ export const loginSchema = z.object({
   phone: z.string({
     required_error: "Số điện thoại là bắt buộc",
   })
+    .regex(phoneRegex, "Số điện thoại chỉ chứa các chữ số")
     .min(10, "Số điện thoại phải có ít nhất 10 số")
-    .max(11, "Số điện thoại không được quá 11 số")
-    .regex(phoneRegex, "Số điện thoại chỉ được chứa số"),
+    .max(11, "Số điện thoại không được quá 11 số"),
   password: z.string({
     required_error: "Mật khẩu là bắt buộc",
   })
+    .regex(passwordRegex, "Mật khẩu phải bắt đầu bằng chữ hoa")
     .min(6, "Mật khẩu phải có ít nhất 6 ký tự")
     .max(8, "Mật khẩu không được quá 8 ký tự")
-    .regex(passwordRegex, "Mật khẩu phải bắt đầu bằng chữ in hoa")
 });
 
 export const registerSchema = loginSchema.extend({
